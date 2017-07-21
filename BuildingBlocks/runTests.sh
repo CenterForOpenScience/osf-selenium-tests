c79e5182cec6ca8763d14e35a4e292c07e4e229e
Patrick Anderson <psa5dg@virginia.edu>
2017-07-21 13:27:36 -0400

    random stuff

--- BuildingBlocks/runTests.sh
+++ BuildingBlocks/runTests.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 echo "hello, $USER. Running a few tests in BrowserStack..."
 python Login.py
-python CreateComponent.py
-python BottomBanner.py
-python AddonEnable.py
-python DisableAddons.py
-python DisconnectAllAccounts.py
-python Login.py
-python Login.py
-python Login.py
-python Login.py
-python Login.py
-python Login.py
-python Login.py
--- Sandbox/test.sh
+++ Sandbox/test.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 echo "hello, $USER. Running a few tests in BrowserStack..."
 python patrickTest.py
-python AmandaNewTest.py
+python patrickTest.py
+python patrickTest.py
+python patrickTest.py
+python patrickTest.py
+python patrickTest.py
+python patrickTest.py
+python patrickTest.py
+python patrickTest.py
+python patrickTest.py
+python patrickTest.py
+python patrickTest.py
