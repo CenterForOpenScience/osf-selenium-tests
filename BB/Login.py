a9f437d67f7e28efd40c3efa84d84f25ff7d2a37
sdubey6 <sdubey6@uic.edu>
2017-08-10 14:59:20 -0400

    Add files via upload

---
+++ BB/Login.py
@@ -0,0 +1,46 @@
+'''
+Created on Aug 10, 2017
+
+@author: shikhadubey
+'''
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from threading import Thread
+import time#
+from selenium.webdriver.support.select import Select
+from time import sleep
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+
+class Login:
+   
+    def staging_login(self, driver):
+        driver.get("https://staging.osf.io/")
+        time.sleep(3)
+        driver.find_element_by_partial_link_text("Sign In").click()
+        time.sleep(3)
+        driver.find_element_by_id("username").send_keys("osframeworktesting+ghost@gmail.com")
+        time.sleep(3)
+        driver.find_element_by_id('password').send_keys('\"Repr0duce!\"')
+        time.sleep(3)
+        driver.implicitly_wait(10)
+        if (driver.find_element_by_id("rememberMe").is_selected()):
+            driver.find_element_by_id("rememberMe").click()
+            
+        driver.find_element_by_name("submit").click()
+
+    def production_login(self, driver):
+        driver.get("https://osf.io/")
+        time.sleep(3)
+        driver.find_element_by_partial_link_text("Sign In").click()
+        time.sleep(3)
+        driver.find_element_by_id("username").send_keys("osframeworktesting+ghost@gmail.com")
+        time.sleep(3)
+        driver.find_element_by_id('password').send_keys('\"Repr0duce!\"')
+        time.sleep(3)
+        driver.implicitly_wait(10)
+        if (driver.find_element_by_id("rememberMe").is_selected()):
+            driver.find_element_by_id("rememberMe").click()
+            
+        driver.find_element_by_name("submit").click()
