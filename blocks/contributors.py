0c8719af9d0418a22b7ec750640a5e7ab45d8e55
sdubey6 <sdubey6@uic.edu>
2017-08-29 12:14:09 -0500

    Update contributors.py

--- blocks/contributors.py
+++ blocks/contributors.py
@@ -8,7 +8,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 import time
 
 class Contributors:
-    def search_contributor(self, driver):
+    def search_add_contributor(self, driver):
        
         driver.find_element_by_css_selector("#secondary-navigation > ul > li:nth-child(1) > a").click()
         time.sleep(3)
