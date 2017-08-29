57e02fd8a3d26ab4a9117c76183b5f45573f8759
sdubey6 <sdubey6@uic.edu>
2017-08-29 14:10:56 -0500

    Update test_contributors.py

--- Staging/test_contributors.py
+++ Staging/test_contributors.py
@@ -21,5 +21,5 @@ def test_contributors():
     #driver.find_element_by_css_selector("#projectSubnav > div > div.collapse.navbar-collapse.project-nav > ul > li.active > a").click()
     c.changetoread_contributor(driver)
     c.reorder_contributor(driver)
-    p.delete_node(driver, project_id + 'settings/')
+    #p.delete_node(driver, project_id + 'settings/')
     driver.quit()
