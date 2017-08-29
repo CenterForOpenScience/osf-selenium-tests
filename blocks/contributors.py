000b2892d64fe9d25138138e397bb447a23028ee
sdubey6 <sdubey6@uic.edu>
2017-08-29 13:19:00 -0500

    Update contributors.py

--- blocks/contributors.py
+++ blocks/contributors.py
@@ -36,5 +36,6 @@ class Contributors:
        
         driver.find_element_by_css_selector("#contributors > tr:nth-child(2) > td.permissions > div.td-content > span:nth-child(1) > select").click()
         element= driver.find_element_by_css_selector("#contributors > tr:nth-child(2) > td.permissions > div.td-content > span:nth-child(1) > select").send_keys("read")
-        element.send_keys(Keys.ENTER)
-        driver.find_element_by_css_selector("button[type=\"button\"].btn.btn-success").click()
+        #element.send_keys(Keys.ENTER)
+        element.click()
+        #driver.find_element_by_css_selector("button[type=\"button\"].btn.btn-success").click()
