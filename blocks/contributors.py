b9fb7eb77781e54be72e98611544014c0bf08e75
sdubey6 <sdubey6@uic.edu>
2017-08-29 11:32:29 -0500

    Update contributors.py

--- blocks/contributors.py
+++ blocks/contributors.py
@@ -32,3 +32,4 @@ class Contributors:
         if not (len(driver.find_elements_by_css_selector("#contributors > tr:nth-child(2)")) != 0):
             raise Exception("assertElementPresent failed")
         driver.find_element_by_css_selector("#addContributors > div > div > div.modal-footer > span:nth-child(3) > a.btn.btn-success").click()
+        #test
