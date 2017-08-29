844d2e8ac5b08e6f0ce9475a77edc178767fa7e3
sdubey6 <sdubey6@uic.edu>
2017-08-29 10:41:28 -0500

    Update test_contributors.py

--- Staging/test_contributors.py
+++ Staging/test_contributors.py
@@ -4,7 +4,7 @@ from blocks.nodes import Nodes
 from selenium import webdriver
 from blocks.contributors import Contributors
 
-desired_cap = {'browser': 'Chrome', 'browser_version': '59.0', 'os': 'OS X', 'os_version': 'Sierra', 'resolution': '1920x1080'}
+desired_cap = {'browser': 'Chrome', 'browser_version': '60.0', 'os': 'Windows', 'os_version': '10', 'resolution': '2048x1536'}
 driver = webdriver.Remote(
 command_executor='http://osfselenium1:9asHrZGoyk7Tesx9agX5@hub.browserstack.com:80/wd/hub',
 desired_capabilities=desired_cap)
