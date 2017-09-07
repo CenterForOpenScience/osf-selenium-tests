f5f11250756d1308416a3546c66d70807a1f53fe
Patrick Anderson <psa5dg@virginia.edu>
2017-09-07 15:01:20 -0400

    sign in

--- Staging/test_sign_in.py
+++ Staging/test_sign_in.py
@@ -8,28 +8,39 @@ desired_capabilities=desired_cap)
 
 def test_landing_page():
 
+	# basically clicks on every single button on the page.
 	driver.get("https://staging.osf.io/")
-	#time.sleep(3)
 	driver.find_element_by_xpath('//*[@id="secondary-navigation"]/ul/li[4]/div/a[2]').click()
-	#time.sleep(3)
 	driver.find_element_by_xpath('//*[@id="rememberMe"]').click()
 	driver.find_element_by_xpath('//*[@id="rememberMe"]').click()
 	driver.find_element_by_xpath('//*[@id="forgot-password"]').click()
-	#time.sleep(3)
 	assert driver.current_url == 'https://staging.osf.io/forgotpassword/'
 	driver.get('https://staging-accounts.osf.io/login?service=https://staging.osf.io/')
 	driver.find_element_by_xpath('//*[@id="alternative-institution"]').click()
-	#time.sleep(3)
 	assert driver.current_url == 'https://staging-accounts.osf.io/login?campaign=institution&service=https%3A%2F%2Fstaging.osf.io%2F'
 	driver.get('https://staging-accounts.osf.io/login?service=https://staging.osf.io/')
 	driver.find_element_by_xpath('//*[@id="back-to-osf"]').click()
-	#time.sleep(3)
 	assert driver.current_url == 'https://staging.osf.io/'
 	driver.get('https://staging-accounts.osf.io/login?service=https://staging.osf.io/')
 	driver.find_element_by_xpath('//*[@id="fm1"]/section[5]/a').click()
-	#time.sleep(3)
 	assert driver.current_url == 'https://orcid.org/oauth/signin?client_id=APP-0D8MTONZ6PGNNIRH&scope=%2Fauthenticate&response_type=code&redirect_uri=https%3A%2F%2Fstaging-accounts.osf.io%2Flogin%3Fclient_name%3DOrcidClient#show_login'
 	driver.get('https://staging-accounts.osf.io/login?service=https://staging.osf.io/')
 
-	# use various emails now        
+
+	# tries a primary email and a secondary email.
+	driver.find_element_by_xpath('//*[@id="username"]').send_keys('osframeworktesting+ghost@gmail.com')
+	driver.find_element_by_xpath('//*[@id="password"]').send_keys('"Repr0duce!"')
+	driver.find_element_by_xpath('//*[@id="fm1"]/section[3]/input[4]').click()
+	time.sleep(5)
+	assert driver.find_element_by_xpath('//*[@id="osfHome"]/div[3]/div/div/div/div/div[1]/h2')
+	driver.find_element_by_xpath('//*[@id="secondary-navigation"]/ul/li[5]/button').click()
+	driver.find_element_by_xpath('//*[@id="secondary-navigation"]/ul/li[5]/ul/li[4]/a').click()
+
+	driver.get('https://staging-accounts.osf.io/login?service=https://staging.osf.io/')
+	driver.find_element_by_xpath('//*[@id="username"]').send_keys('osframeworktesting+ghost2@gmail.com')
+	driver.find_element_by_xpath('//*[@id="password"]').send_keys('"Repr0duce!"')
+	driver.find_element_by_xpath('//*[@id="fm1"]/section[3]/input[4]').click()
+	time.sleep(5)
+	assert driver.find_element_by_xpath('//*[@id="osfHome"]/div[3]/div/div/div/div/div[1]/h2')
+
 	driver.quit()
