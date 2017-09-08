a23473aad3ed1ed513fe1bcf7ac0bd404eaf17b4
sdubey6 <sdubey6@uic.edu>
2017-09-08 08:39:19 -0500

    Update search.py

--- blocks/search.py
+++ blocks/search.py
@@ -11,5 +11,6 @@ import time
 class Search:
 
     def staging_nav_bar(self, driver):
+        driver.sleep(3)
         driver.get("https://staging.osf.io/")
         driver.find_element_by_xpath("//*[@id="navbarScope"]/div/div[1]/div[1]/a/span[2]/strong").click()
