3d531aa04647b1eefb8a8e3d738dc198729afad7
sdubey6 <sdubey6@uic.edu>
2017-09-08 09:39:24 -0500

    Update search.py

--- blocks/search.py
+++ blocks/search.py
@@ -5,7 +5,7 @@ from threading import Thread
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-import time
+from time import sleep
 
 
 class Search:
