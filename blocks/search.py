646fe39281001f0b7e5ff1d63fa48f64ed2bf0cb
sdubey6 <sdubey6@uic.edu>
2017-09-08 09:30:46 -0500

    Update search.py

--- blocks/search.py
+++ blocks/search.py
@@ -12,5 +12,4 @@ class Search:
 
     def staging_nav_bar(self, driver):
         driver.sleep(3)
-        driver.get("https://staging.osf.io/")
-        //driver.find_element_by_xpath("//*[@id="navbarScope"]/div/div[1]/div[1]/a/span[2]/strong").click()
+        
