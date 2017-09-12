d3ffbc3c037b013297584de96208b216153833f8
sdubey6 <sdubey6@uic.edu>
2017-09-12 10:26:48 -0500

    Update search.py

--- blocks/search.py
+++ blocks/search.py
@@ -28,5 +28,8 @@ class Search:
         time.sleep(3)
         driver.find_element_by_css_selector("#primary-navigation > span").click()
         driver.find_element_by_css_selector("#navbarScope > div > div.navbar-header > div.dropdown.primary-nav.open > ul > li:nth-child(3) > a > b").click()
-        assert "https://staging.osf.io/registries/" in driver.title
-        
+        #assert "https://staging.osf.io/registries/" in driver.title
+        driver.find_element_by_css_selector("#primary-navigation > span").click()
+        driver.find_element_by_css_selector("#navbarScope > div > div.navbar-header > div.dropdown.primary-nav.open > ul > li:nth-child(4) > a > b").click()
+        time.sleep(2)
+        driver.back()
