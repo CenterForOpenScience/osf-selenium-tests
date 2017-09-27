7482c2b9ff3c71e070e5f9b51ab57a9109f75151
sdubey6 <sdubey6@uic.edu>
2017-09-27 09:39:10 -0500

    Update __init__.py

--- settings/__init__.py
+++ settings/__init__.py
@@ -1,7 +1,7 @@
-from .defaults import *
+#from .defaults import *
 
 
-try:
-    from .local import *  # noqa
-except ImportError as error:
-    raise ImportError("No local.py settings file found. Did you remember to add your local.py?")
+#try:
+    #from .local import *  # noqa
+#except ImportError as error:
+    #raise ImportError("No local.py settings file found. Did you remember to add your local.py?")
