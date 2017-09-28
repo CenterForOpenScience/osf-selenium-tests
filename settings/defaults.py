4dfcc7c7de8b9f44a78acf4dd3681d4098f620bc
sdubey6 <sdubey6@uic.edu>
2017-09-28 09:09:49 -0500

    Update defaults.py

--- settings/defaults.py
+++ settings/defaults.py
@@ -7,6 +7,6 @@ USERNAME_TWO = ''
     
 PASSWORD = ''
     
-DESIRED_CAP = {'browser': 'Chrome', 'browser_version': '61.0', 'os': 'Windows', 'os_version': '10', 'resolution': '2048x1536'}
+DESIRED_CAP = {'browser': 'Chrome', 'browser_version': '61.0', 'os': 'Windows', 'os_version': '10', 'resolution': '1920x1080'}
     
 DRIVER = None
