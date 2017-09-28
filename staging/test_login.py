7735a7a20b458fb3e1616b09da7da555422af11b
Patrick Anderson <psa5dg@virginia.edu>
2017-09-28 16:12:39 -0400

    Formatted login

--- staging/test_login.py
+++ staging/test_login.py
@@ -4,8 +4,10 @@ from helpers import login
 
 driver = settings.DRIVER
 
+
 def test_login():
     login.login(driver)
     time.sleep(5)
-    assert driver.find_element_by_xpath("//*[@id='osfHome']/div[3]/div/div/div/div/div[1]/h2")
+    assert driver.find_element_by_xpath(
+        "//*[@id='osfHome']/div[3]/div/div/div/div/div[1]/h2")
     driver.quit()
