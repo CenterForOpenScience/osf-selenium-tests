ba2d44a7e04ac178338470ab9e17d0c833182eb3
sdubey6 <sdubey6@uic.edu>
2017-10-10 10:50:32 -0500

    Update test_meetings.py

--- staging/test_meetings.py
+++ staging/test_meetings.py
@@ -13,5 +13,28 @@ driver = settings.DRIVER
 def test_meetings():
     #login.login(driver)
     driver.get("https://staging.osf.io/meetings/")
-    search.staging_navbar(driver)
+    time.sleep(3)
+    driver.find_element_by_css_selector("#primary-navigation > span").click()
+    time.sleep(2)
+    driver.find_element_by_css_selector("#navbarScope > div > div.navbar-header > div.dropdown.primary-nav.open > ul > li:nth-child(1) > a > b").click()
+    assert "https://staging.osf.io/" in driver.current_url
+    time.sleep(3)
+    driver.find_element_by_css_selector("#primary-navigation > span").click()
+    time.sleep(1)
+    driver.find_element_by_css_selector("#navbarScope > div > div.navbar-header > div.dropdown.primary-nav.open > ul > li:nth-child(2) > a > b").click()
+    assert "https://staging.osf.io/preprints/" in driver.current_url
+    time.sleep(2)
+    driver.back() 
+    time.sleep(3)
+    driver.find_element_by_css_selector("#primary-navigation > span").click()
+    driver.find_element_by_css_selector("#navbarScope > div > div.navbar-header > div.dropdown.primary-nav.open > ul > li:nth-child(3) > a > b").click()
+    assert "https://staging.osf.io/registries/" in driver.current_url
+    time.sleep(2)
+    driver.back()
+    time.sleep(2)
+    driver.find_element_by_css_selector("#primary-navigation > span").click()
+    driver.find_element_by_css_selector("#navbarScope > div > div.navbar-header > div.dropdown.primary-nav.open > ul > li:nth-child(4) > a > b").click()
+    assert "https://staging.osf.io/meetings" in driver.current_url
+    time.sleep(2)
+    driver.back()
 
