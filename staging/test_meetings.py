8378aedbe1fd140cb332a5fea98772665d9e55dd
sdubey6 <sdubey6@uic.edu>
2017-10-10 10:06:50 -0500

    Update test_meetings.py

--- staging/test_meetings.py
+++ staging/test_meetings.py
@@ -4,11 +4,13 @@ from helpers import login
 from helpers import forks
 from helpers import nodes
 from helpers import meetings
+from staging import test_landing_page
 
 #driver = DRIVER
 driver = settings.DRIVER
 #DRIVER= webdriver.Remote(command_executor, desired_capabilities=DESIRED_CAP)
 
 def test_meetings():
-    login.login(driver)
+    #login.login(driver)
+    test_landing_page.test_landing_page()
 
