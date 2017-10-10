5d2877d77ac533889ba6ab75fe07f44de7bd2982
sdubey6 <sdubey6@uic.edu>
2017-10-10 09:04:48 -0500

    Create test_meetings.py

---
+++ staging/test_meetings.py
@@ -0,0 +1,12 @@
+import os
+from helpers import login
+from helpers import forks
+from helpers import nodes
+from helpers import contributors
+
+#driver = DRIVER
+
+#DRIVER= webdriver.Remote(command_executor, desired_capabilities=DESIRED_CAP)
+
+def test_meetings():
+
