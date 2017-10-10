0a53c5d4c862cb6afb0314d0ee23ef55dd46fd04
sdubey6 <sdubey6@uic.edu>
2017-10-10 08:16:38 -0500

    Update contributors.py

--- helpers/contributors.py
+++ helpers/contributors.py
@@ -13,8 +13,9 @@ import os
 #DRIVER= webdriver.Remote(command_executor, desired_capabilities=DESIRED_CAP)
 #print(shikha)
 CE = os.environ['CE']
-DESIRED_CAP= os.environ['DESIRED_CAP']
-DRIVER=webdriver.Remote({CE},desired_capabilities={DESIRED_CAP})
+#DESIRED_CAP= os.environ['DESIRED_CAP']
+DC = {'browser': 'Chrome', 'browser_version': '61.0', 'os': 'Windows', 'os_version': '10', 'resolution': '2048x1536'}
+DRIVER=webdriver.Remote({CE},desired_capabilities=DC)
 def search_add_contributor(driver):
    
     driver.find_element_by_css_selector("#secondary-navigation > ul > li:nth-child(1) > a").click()
