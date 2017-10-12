4d611a33db0269badaf12224afe8e582d628c5c9
chennan47 <nc6r@virginia.edu>
2017-10-12 17:18:11 -0400

    finish selenium top navigation bar test and add some utils function

--- SEL/utils.py
+++ SEL/utils.py
@@ -2,6 +2,7 @@ import os
 
 # Selenium imports
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 
 # Project imports
 import settings
@@ -43,14 +44,66 @@ def launch_driver(
     # Return driver
     return driver
 
+
 #TODO use api to get the user info to form a test user which should have everything like a real osf user
 def create_user():
     pass
+
 
 #TODO user api to delete the user after the test is done.
 def delete_user(user):
     pass
 
 
+find_btn = lambda elm: elm.find_element_by_xpath('.//button')
+
+
+def fill_form(
+        root,
+        fields,
+        button_finder=find_btn):
+    """Fill out form fields and click submit button.
+
+    Args:
+        form : root element
+        fields : dict of id -> value pairs for form
+        button_finder : function to get button from root element
+
+    """
+    # Enter field values
+    for field in fields:
+        root.find_element_by_css_selector(field).send_keys(fields[field])
+
+    # Click submit button
+    button_finder(root).click()
+
+
 def login(driver, user):
-    
+    driver.get(settings.osf_login_page)
+    login_form = driver.find_element_by_id('fm1')
+
+    fill_form(
+        login_form,
+        {
+            '#username': user['username'],
+            '#password': user['password'],
+        }
+    )
+
+
+def logout(driver):
+    """ Log out of OSF.
+
+    Args:
+        driver : selenium.webdriver instance
+
+    """
+
+    # locate and click logout button
+    try:
+        driver.find_element_by_xpath(
+            "//div[@id='secondary-navigation']/ul/li[@class='dropdown']/ul/li/a[@href='/logout/']"
+        ).click()
+    except NoSuchElementException:
+        # There is no logout link - assume the user is not logged in
+        pass
