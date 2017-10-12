b69123601cd4d5ea36c404da506ef88172d0290e
sdubey6 <sdubey6@uic.edu>
2017-10-12 09:27:02 -0500

    Update test_meetings.py

--- staging/test_meetings.py
+++ staging/test_meetings.py
@@ -5,6 +5,7 @@ from helpers import forks
 from helpers import nodes
 from helpers import meetings
 from helpers import search
+from helpers import search
 
 #driver = DRIVER
 driver = settings.DRIVER
@@ -14,4 +15,5 @@ def test_meetings():
     meetings.osf_meetings_landing_page(driver)
     meetings.osf_meetings_sign_in(driver)
     meetings.osf_meetings_search_meeting(driver)
+    meetings.osf_meeting_bottom_bar(driver)
     
