3da5ebc72660afb969e63fc115bd4f776b810ad8
Fabrice Mizero <fabmizz@gmail.com>
2017-10-13 13:29:01 -0400

    Fix bstack creds

--- helpers/contributors.py
+++ helpers/contributors.py
@@ -8,16 +8,15 @@ from selenium.webdriver.support.ui import WebDriverWait
 import time
 from selenium.webdriver.common.action_chains import ActionChains
 import os
+import settings
 
 #command_executor = os.environ['command_executor']
 #DRIVER= webdriver.Remote(command_executor, desired_capabilities=DESIRED_CAP)
 #print(shikha)
-CE = os.environ['CE']
 #DESIRED_CAP= os.environ['DESIRED_CAP']
-DC = {'browser': 'Chrome', 'browser_version': '61.0', 'os': 'Windows', 'os_version': '10', 'resolution': '2048x1536'}
-DRIVER=webdriver.Remote({CE},desired_capabilities=DC)
+DRIVER = settings.DRIVER
 def search_add_contributor(driver):
-   
+
     driver.find_element_by_css_selector("#secondary-navigation > ul > li:nth-child(1) > a").click()
     time.sleep(3)
     driver.find_element_by_partial_link_text("Testselenium").click()
@@ -41,9 +40,9 @@ def search_add_contributor(driver):
     driver.find_element_by_css_selector("#addContributors > div > div > div.modal-body > div:nth-child(1) > div > div:nth-child(1) > table > tbody > tr:nth-child(1) > td.p-r-sm.osf-icon-td > a > i").click()
     driver.find_element_by_css_selector("#addContributors > div > div > div.modal-footer > span:nth-child(3) > a.btn.btn-success").click()
     time.sleep(3)
-    
+
 def changetoread_contributor(driver):
-   
+
     driver.find_element_by_css_selector("#contributors > tr:nth-child(2) > td.permissions > div.td-content > span:nth-child(1) > select").click()
     element= driver.find_element_by_css_selector("#contributors > tr:nth-child(2) > td.permissions > div.td-content > span:nth-child(1) > select").send_keys("read", Keys.ENTER)
     #element.send_keys(Keys.ENTER)
--- helpers/meetings.py
+++ helpers/meetings.py
@@ -30,7 +30,7 @@ def osf_meetings_landing_page(driver):
     driver.find_element_by_css_selector("#navbarScope > div > div.navbar-header > div.dropdown.primary-nav.open > ul > li:nth-child(2) > a > b").click()
     assert "https://staging.osf.io/preprints/" in driver.current_url
     time.sleep(2)
-    driver.back() 
+    driver.back()
     time.sleep(3)
     driver.find_element_by_css_selector("#primary-navigation > span").click()
     driver.find_element_by_css_selector("#navbarScope > div > div.navbar-header > div.dropdown.primary-nav.open > ul > li:nth-child(3) > a > b").click()
@@ -41,7 +41,7 @@ def osf_meetings_landing_page(driver):
     driver.find_element_by_css_selector("#navbarScope > div > div.navbar-header > div.dropdown.primary-nav.open > ul > li:nth-child(4) > a > b").click()
     assert "https://staging.osf.io/meetings" in driver.current_url
     time.sleep(2)
-  
+
     driver.find_element_by_css_selector("#secondary-navigation > ul > li:nth-child(2) > a").click()
     #assert "https://staging.osf.io/support/" in driver.current_url
     driver.back()
@@ -50,14 +50,14 @@ def osf_meetings_landing_page(driver):
     assert "https://cos.io/donate-to-cos/" in driver.current_url
     time.sleep(2)
     driver.back()
-    
+
 def osf_meetings_sign_in(driver):
-    
+
     driver.find_element_by_partial_link_text("Sign In").click()
     driver.find_element_by_id("username").send_keys(settings.USERNAME_ONE)
     driver.find_element_by_id('password').send_keys(settings.PASSWORD)
     if (driver.find_element_by_id("rememberMe").is_selected()):
-        driver.find_element_by_id("rememberMe").click() 
+        driver.find_element_by_id("rememberMe").click()
     driver.find_element_by_name("submit").click()
     driver.find_element_by_css_selector("body > div.watermarked > div.osf-meeting-header-img > div > div > div.row > div:nth-child(1) > div.p-v-md > button").click()
     time.sleep(2)
@@ -72,10 +72,8 @@ def osf_meetings_sign_in(driver):
     e3 = driver.find_element_by_css_selector("#osf-meeting-upload > div > ul > li:nth-child(2)")
     assert e3.text == "Send your materials to the OSF for Meetings email address for your conference"
     #e4 = driver.find_element_by_css_selector("#osf-meeting-upload > div > p:nth-child(3)")
-    
-    
+
+
 def osf_meetings_search_meeting(driver):
     driver.find_element_by_css_selector("#meetings-grid > div > div > div.tb-head > div > input").send_keys("osf for meetings 2015", Keys.ENTER)
     assert driver.find_element_by_link_text("OSF for Meetings 2015").is_displayed()
-    
-    
