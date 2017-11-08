5e7ce696305651ed00992b4e56e9005e85e71154
Fabrice Mizero <fabmizz@gmail.com>
2017-11-08 10:38:52 -0500

    Pin Chrome to v61, Safari to v10

--- settings/local-travis.py
+++ settings/local-travis.py
@@ -12,13 +12,13 @@ USER_TWO_PASSWORD = os_env.get('USER_TWO_PASSWORD')
 DRIVER = 'Remote'
 
 caps = {
-    'chrome': {'browser': 'Chrome', 'os': 'Windows', 'os_version': '10', 'resolution': '2048x1536'},
+    'chrome': {'browser': 'Chrome', 'browser_version': '61.0', 'os': 'Windows', 'os_version': '10', 'resolution': '2048x1536'},
     'edge': {'browser': 'Edge', 'os': 'Windows', 'os_version': '10', 'resolution': '2048x1536'},
     'firefox': {'browser': 'Firefox', 'os': 'Windows', 'os_version': '10', 'resolution': '2048x1536'},
     'msie': {'browser': 'IE', 'os': 'Windows', 'os_version': '7', 'resolution': '2048x1536'},
     'android': {'device': 'Samsung Galaxy S8', 'realMobile': 'true', 'os_version': '7.0'},
     'ios': {'device': 'iPhone 7', 'realMobile': 'true', 'os_version': '10.0'},
-    'safari': {'browser': 'Safari', 'browser_version': '10.1', 'os': 'OS X', 'os_version': 'Sierra', 'resolution': '1024x768'}
+    'safari': {'browser': 'Safari', 'browser_version': '10', 'os': 'OS X', 'os_version': 'Sierra', 'resolution': '1024x768'}
 }
 
 BUILD = os_env.get('TEST_BUILD', 'firefox')
