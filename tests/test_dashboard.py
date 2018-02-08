2af5c3db8e28618791a87c591cace5785cfc7e30
Lauren Revere <laurenrevere@cos.io>
2018-02-08 10:14:26 -0500

    Complete dashboard tests

--- tests/test_dashboard.py
+++ tests/test_dashboard.py
@@ -1,10 +1,12 @@
 import pytest
 
-from tests.base import SeleniumTest
 from api import osf_api as osf
-
+from tests.base import SeleniumTest
 from pages.project import ProjectPage
+from pages.meeting import MeetingPage
+from pages.preprint import PreprintPage
 from pages.dashboard import DashboardPage
+from pages.prereg import PreregLandingPage
 
 
 class TestDashboardPage(SeleniumTest):
@@ -12,6 +14,10 @@ class TestDashboardPage(SeleniumTest):
     def setup_method(self, method):
         self.dashboard_page = DashboardPage(self.driver)
         self.dashboard_page.goto()
+
+    #TODO: Decide if deleting all projects should go somewhere else
+    def teardown_method(self, method):
+        osf.delete_all_user_projects(session=self.session)
 
     def test_create_project(self):
         project_title = 'New Project'
@@ -25,30 +31,55 @@ class TestDashboardPage(SeleniumTest):
         project_page = ProjectPage(self.driver, verify=True)
         assert project_page.project_title.text == project_title, 'Project title incorrect.'
 
-    def test_modal_buttons(self):
-        #TODO: Get user institutions from user object
+    def test_create_project_modal_buttons(self):
+        institutions = osf.get_user_institutions(self.session)
         self.dashboard_page.create_project_button.click()
 
         create_project_modal = self.dashboard_page.CreateProjectModal(self.driver)
+
+        create_project_modal.remove_all_link.click()
+        for institution in institutions:
+            assert not create_project_modal.institution_selected(institution)
+        create_project_modal.select_all_link.click()
+        for institution in institutions:
+            assert create_project_modal.institution_selected(institution)
 
         create_project_modal.more_arrow.click()
         assert create_project_modal.description_input, 'Description input missing.'
         assert create_project_modal.template_dropdown, 'Template dropdown missing.'
         create_project_modal.more_arrow.click()
-        with pytest.raises(ValueError):
-            create_project_modal.description_input
-        with pytest.raises(ValueError):
-            create_project_modal.template_dropdown
-
-        #TODO: Add back when institutions can be grabbed from user
-        # create_project_modal.remove_all_link.click()
-        # assert not create_project_modal.institutions_are_selected(institutions)
-        # create_project_modal.select_all_link.click()
-        # assert create_project_modal.institutions_are_selected(institutions)
+        assert create_project_modal.invisible('description_input')
+        assert create_project_modal.invisible('template_dropdown')
 
         create_project_modal.cancel_button.click()
-        with pytest.raises(ValueError):
-            create_project_modal.modal
+
+        assert create_project_modal.invisible('modal')
+
+    def test_institution_logos(self):
+        # TODO: This will not work on production - we don't put up all logos
+        api_institution_names = osf.get_all_institutions(self.session)
+        page_institutions = self.dashboard_page.get_institutions()
+        page_institution_names = [i.get_property('name') for i in page_institutions]
+        assert set(page_institution_names) == set(api_institution_names)
+
+    def test_new_and_noteworthy(self):
+        # TODO: Possibly write this to fail gracefully with assertions
+        # Check if new and noteworthy and public projects are loaded
+        self.dashboard_page.new_and_noteworthy
+        self.dashboard_page.popular_projects
+
+    def test_meetings_link(self):
+        self.dashboard_page.view_meetings_button.click()
+        assert MeetingPage(self.driver).verify()
+
+    def test_preprints_link(self):
+        self.dashboard_page.view_preprints_button.click()
+        assert PreprintPage(self.driver).verify()
+
+    def test_prereg_link(self):
+        self.dashboard_page.start_prereg_button.click()
+        assert PreregLandingPage(self.driver).verify()
+
 
 class TestDashboardPageProjectList(SeleniumTest):
 
@@ -74,7 +105,7 @@ class TestDashboardPageProjectList(SeleniumTest):
         yield project_three
         project_three.delete()
 
-    def test_sorting(self, project_one, project_two, project_three):
+    def test_project_sorting(self, project_one, project_two, project_three):
         self.dashboard_page.reload()
 
         project_list = self.dashboard_page.ProjectList(self.driver)
@@ -83,43 +114,43 @@ class TestDashboardPageProjectList(SeleniumTest):
 
         assert 'selected' in project_list.sort_date_dsc_button.get_attribute('class')
         assert 'not-selected' in project_list.sort_date_asc_button.get_attribute('class')
-        assert project_three.id in project_list.get_nth_project(self.driver, 1)['guid']
-        assert project_two.id in project_list.get_nth_project(self.driver, 2)['guid']
-        assert project_one.id in project_list.get_nth_project(self.driver, 3)['guid']
+        assert project_three.id in project_list.get_nth_project(1)['guid']
+        assert project_two.id in project_list.get_nth_project(2)['guid']
+        assert project_one.id in project_list.get_nth_project(3)['guid']
 
         project_list.sort_date_asc_button.click()
         assert 'selected' in project_list.sort_date_asc_button.get_attribute('class')
         assert 'not-selected' in project_list.sort_date_dsc_button.get_attribute('class')
-        assert project_one.id in project_list.get_nth_project(self.driver, 1)['guid']
-        assert project_two.id in project_list.get_nth_project(self.driver, 2)['guid']
-        assert project_three.id in project_list.get_nth_project(self.driver, 3)['guid']
+        assert project_one.id in project_list.get_nth_project(1)['guid']
+        assert project_two.id in project_list.get_nth_project(2)['guid']
+        assert project_three.id in project_list.get_nth_project(3)['guid']
 
         project_list.sort_title_asc_button.click()
         assert 'selected' in project_list.sort_title_asc_button.get_attribute('class')
         assert 'not-selected' in project_list.sort_title_dsc_button.get_attribute('class')
-        assert project_one.id in project_list.get_nth_project(self.driver, 1)['guid']
-        assert project_three.id in project_list.get_nth_project(self.driver, 2)['guid']
-        assert project_two.id in project_list.get_nth_project(self.driver, 3)['guid']
+        assert project_one.id in project_list.get_nth_project(1)['guid']
+        assert project_three.id in project_list.get_nth_project(2)['guid']
+        assert project_two.id in project_list.get_nth_project(3)['guid']
 
         project_list.sort_title_dsc_button.click()
         assert 'selected' in project_list.sort_title_dsc_button.get_attribute('class')
         assert 'not-selected' in project_list.sort_title_asc_button.get_attribute('class')
-        assert project_two.id in project_list.get_nth_project(self.driver, 1)['guid']
-        assert project_three.id in project_list.get_nth_project(self.driver, 2)['guid']
-        assert project_one.id in project_list.get_nth_project(self.driver, 3)['guid']
+        assert project_two.id in project_list.get_nth_project(1)['guid']
+        assert project_three.id in project_list.get_nth_project(2)['guid']
+        assert project_one.id in project_list.get_nth_project(3)['guid']
 
-    def test_quick_search(self, project_one, project_two, project_three):
+    def test_project_quick_search(self, project_one, project_two, project_three):
         self.dashboard_page.reload()
 
         project_list = self.dashboard_page.ProjectList(self.driver)
 
         project_list.search_input.clear()
         project_list.search_input.send_keys('&&aaaa')
-        assert project_list.get_list_length(self.driver) == 3
+        assert project_list.get_list_length() == 3
 
         project_list.search_input.send_keys('a')
-        assert project_list.get_list_length(self.driver) == 2
+        assert project_list.get_list_length() == 2
 
         project_list.search_input.send_keys('a')
-        assert project_list.get_list_length(self.driver) == 1
-        assert project_one.id in project_list.get_nth_project(self.driver, 1)['guid']
+        assert project_list.get_list_length() == 1
+        assert project_one.id in project_list.get_nth_project(1)['guid']
