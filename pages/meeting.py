8d23c2a386fd69e32bc75d15d6b6557493933c15
ajski <alisc262@gmail.com>
2018-02-14 14:44:09 -0500

    Add meetings landing page test

--- pages/meeting.py
+++ pages/meeting.py
@@ -9,7 +9,9 @@ class MeetingPage(OSFBasePage):
     # Locators
     identity = Locator(By.CSS_SELECTOR, 'div.osf-meeting-header-img', settings.LONG_TIMEOUT)
     register_button = Locator(By.CSS_SELECTOR, 'div.osf-meeting-header-img div.osf-meeting-header div button[data-target="#osf-meeting-register"]', settings.LONG_TIMEOUT)
+    register_text = Locator(By.CSS_SELECTOR, '#osf-meeting-register > div:nth-child(1) > p:nth-child(1)')
     upload_button = Locator(By.CSS_SELECTOR, 'div.osf-meeting-header-img div.osf-meeting-header div button[data-target="#osf-meeting-upload"]', settings.LONG_TIMEOUT)
+    upload_text = Locator(By.CSS_SELECTOR, '#osf-meeting-upload > div > ul > li:nth-child(2)')
 
     def __init__(self, driver, verify=False):
         super(MeetingPage, self).__init__(driver, verify)
