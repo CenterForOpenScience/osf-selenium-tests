7aa099c7e69e564edce5e30b66e6d1ada1fffc78
ajski <alisc262@gmail.com>
2018-02-20 16:13:37 -0500

    Update test_meetings_landing and test_meetings_list

--- pages/meetings.py
+++ pages/meetings.py
@@ -7,7 +7,23 @@ from pages.base import OSFBasePage
 from components.navbars import MeetingsNavbar
 
 
-class MeetingsPage(OSFBasePage):
+
+class BaseMeetingsPage(OSFBasePage):
+
+    def __init__(self, driver, verify=False):
+        super(BaseMeetingsPage, self).__init__(driver, verify)
+        self.navbar = self.MeetingsPageNavbar(driver)
+
+    class MeetingsPageNavbar(Navbar):
+
+        # Locators
+        support_link = Locator(By.LINK_TEXT, 'Support')
+
+        def verify(self):
+            return self.current_service.text == 'MEETINGS'
+
+
+class MeetingsPage(BaseMeetingsPage):
     url = settings.OSF_HOME + '/meetings'
 
     identity = Locator(By.CSS_SELECTOR, 'div.osf-meeting-header-img', settings.LONG_TIMEOUT)
@@ -16,6 +32,17 @@ class MeetingsPage(OSFBasePage):
     upload_button = Locator(By.CSS_SELECTOR, 'div.osf-meeting-header-img div.osf-meeting-header div button[data-target="#osf-meeting-upload"]', settings.LONG_TIMEOUT)
     upload_text = Locator(By.CSS_SELECTOR, '#osf-meeting-upload > div > ul > li:nth-child(2)')
     top_meeting_link = Locator(By.CSS_SELECTOR, '#tb-tbody > div > div > div:nth-child(1) > div.tb-td.tb-col-0 > a')
+    aps_logo = Locator(By.CSS_SELECTOR, 'body > div.watermarked > div.container.grey-background > div.row.org-logo.m-b-lg > div:nth-child(1) > a > img')
+    bitss_logo = Locator(By.CSS_SELECTOR, 'body > div.watermarked > div.container.grey-background > div.row.org-logo.m-b-lg > div:nth-child(2) > a > img')
+    nrao_logo = Locator(By.CSS_SELECTOR, 'body > div.watermarked > div.container.grey-background > div.row.org-logo.m-b-lg > div:nth-child(3) > a > img')
+    spsp_logo = Locator(By.CSS_SELECTOR, 'body > div.watermarked > div.container.grey-background > div.row.org-logo.m-b-lg > div:nth-child(4) > a > img')
 
     # Components
     navbar = ComponentLocator(MeetingsNavbar)
+
+class MeetingDetailPage(BaseMeetingsPage):
+    url = settings.OSF_HOME + '/view/'
+
+    # Locators
+    identity = Locator(By.CSS_SELECTOR, '#grid > div > div > div.tb-head > div > input')
+    meeting_title = Locator(By.CSS_SELECTOR, 'body > div.watermarked > div > h2')
--- tests/test_meetings.py
+++ tests/test_meetings.py
@@ -1,26 +1,37 @@
-import pytest
-from pages.meetings import MeetingsPage
+# import pytest
+
+from tests.base import SeleniumTest
+# from api import osf_api as osf
+
+from pages.meetings import MeetingsPage, MeetingDetailPage
 
 
-@pytest.fixture()
-def meetings_page(driver):
-    meetings_page = MeetingsPage(driver)
-    meetings_page.goto()
-    return meetings_page
+class TestMeetingsPage(SeleniumTest):
 
-class TestMeetingsPage:
+    def setup_method(self, method):
+        self.meetings_page = MeetingsPage(self.driver)
+        self.meetings_page.goto()
 
-    def test_meetings_landing(self, meetings_page):
-        meetings_page.register_text.absent()
-        meetings_page.register_button.click()
-        meetings_page.register_text
+    def test_meetings_landing(self):
+        assert self.meetings_page.register_text.absent()
+        self.meetings_page.register_button.click()
+        self.meetings_page.register_text
 
-        meetings_page.upload_text.absent()
-        meetings_page.upload_button.click()
-        meetings_page.upload_text
+        assert self.meetings_page.upload_text.absent()
+        self.meetings_page.upload_button.click()
+        self.meetings_page.upload_text
 
-    # check 4 logos at bottom
+        assert self.meetings_page.aps_logo.present()
+        assert self.meetings_page.bitss_logo.present()
+        assert self.meetings_page.nrao_logo.present()
+        assert self.meetings_page.spsp_logo.present()
 
-    # check clicking a meeting from list and that page loads
+    def test_meetings_list(self):
+        meeting_name = self.meetings_page.top_meeting_link.text
+        self.meetings_page.top_meeting_link.click()
+        meeting_detail = MeetingDetailPage(self.driver)
+        assert self.meetings_page.upload_text.absent()
+        self.driver.switch_to.window(self.driver.window_handles[1])
+        assert meeting_name == meeting_detail.meeting_title.text
 
-    # 2ND TEST (for newly navigated to page) check clicking a meeting project from THAT list and see that project loads
+    # Note for future tests: add 2nd test for a meetings detail page and clicking an individual meeting from that list
