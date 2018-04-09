2b83be4ffd59aa84542484fc311daeedad367b58
Lauren Revere <laurenrevere@cos.io>
2018-04-09 18:19:37 -0400

    Update meetings tests to use ComponentLocator for the navbar

--- pages/meetings.py
+++ pages/meetings.py
@@ -7,24 +7,13 @@ from pages.base import OSFBasePage
 from components.navbars import MeetingsNavbar
 
 
-
 class BaseMeetingsPage(OSFBasePage):
 
-    def __init__(self, driver, verify=False):
-        super(BaseMeetingsPage, self).__init__(driver, verify)
-        self.navbar = self.MeetingsPageNavbar(driver)
-
-    class MeetingsPageNavbar(Navbar):
-
-        # Locators
-        support_link = Locator(By.LINK_TEXT, 'Support')
-
-        def verify(self):
-            return self.current_service.text == 'MEETINGS'
+    navbar = ComponentLocator(MeetingsNavbar)
 
 
 class MeetingsPage(BaseMeetingsPage):
-    url = settings.OSF_HOME + '/meetings'
+    url = settings.OSF_HOME + '/meetings/'
 
     identity = Locator(By.CSS_SELECTOR, 'div.osf-meeting-header-img', settings.LONG_TIMEOUT)
     register_button = Locator(By.CSS_SELECTOR, 'div.osf-meeting-header-img div.osf-meeting-header div button[data-target="#osf-meeting-register"]', settings.LONG_TIMEOUT)
@@ -39,17 +28,14 @@ class MeetingsPage(BaseMeetingsPage):
     nrao_logo = Locator(By.CSS_SELECTOR, 'body > div.watermarked > div.container.grey-background > div.row.org-logo.m-b-lg > div:nth-child(3) > a > img')
     spsp_logo = Locator(By.CSS_SELECTOR, 'body > div.watermarked > div.container.grey-background > div.row.org-logo.m-b-lg > div:nth-child(4) > a > img')
 
-    # Components
-    navbar = ComponentLocator(MeetingsNavbar)
 
 class MeetingDetailPage(BaseMeetingsPage):
     url = settings.OSF_HOME + '/view/'
 
-    # Locators
     identity = Locator(By.CSS_SELECTOR, '#grid > div > div > div.tb-head > div > input')
     meeting_title = Locator(By.CSS_SELECTOR, 'body > div.watermarked > div > h2')
     entry_download_button = Locator(By.CSS_SELECTOR, '#tb-tbody > div > div > div:nth-child(1) > div.tb-td.tb-col-4 > a > button > i')
     second_entry_link = Locator(By.CSS_SELECTOR, '#tb-tbody > div > div > div:nth-child(2) > div.tb-td.tb-col-0 > a')
-    project_title = Locator(By.CSS_SELECTOR, '#nodeTitleEditable')
+    project_title = Locator(By.CSS_SELECTOR, '#nodeTitleEditable', settings.LONG_TIMEOUT)
     filter_input = Locator(By.CSS_SELECTOR, '#grid > div > div > div.tb-head > div > input')
     sort_caret_title_asc = Locator(By.CSS_SELECTOR, '#grid > div > div > div.tb-row-titles > div:nth-child(1) > i.fa.fa-chevron-up.tb-sort-inactive.asc-btn.m-r-xs')
