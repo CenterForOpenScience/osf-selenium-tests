c0932e0842cf0ea11d6da34e30f7b7c78892922a
Lauren Revere <laurenrevere@cos.io>
2018-08-16 11:36:33 -0400

    Remove use of legacy support page

--- pages/support.py
+++ pages/support.py
@@ -6,13 +6,13 @@ from base.locators import Locator
 from pages.base import OSFBasePage
 
 
-class EmberSupportPage(OSFBasePage):
+class SupportPage(OSFBasePage):
     url = settings.OSF_HOME + '/support'
 
     identity = Locator(By.CSS_SELECTOR, '._Support_15i3vw', settings.LONG_TIMEOUT)
 
 
-class SupportPage(OSFBasePage):
-    waffle_override = {'ember_support_page': EmberSupportPage}
+class LegacySupportPage(OSFBasePage):
+    waffle_override = {'ember_support_page': SupportPage}
 
     identity = Locator(By.CSS_SELECTOR, 'body > div.watermarked > div > div > h1', settings.LONG_TIMEOUT)
