73870f7ae97ad9c35f049023ca429cb9e0164355
Lauren Revere <laurenrevere@cos.io>
2018-08-16 10:44:24 -0400

    Add legacy search smoke test

--- pages/search.py
+++ pages/search.py
@@ -2,10 +2,15 @@ import settings
 
 from selenium.webdriver.common.by import By
 
-from base.locators import Locator
 from pages.base import OSFBasePage
+from base.locators import Locator, GroupLocator
 
 class SearchPage(OSFBasePage):
     url = settings.OSF_HOME + '/search/'
 
     identity = Locator(By.ID, 'searchPageFullBar')
+    search_bar = Locator(By.ID, 'searchPageFullBar')
+    loading_indicator = Locator(By.CSS_SELECTOR, '.ball-scale')
+
+    # Group locators
+    search_results = GroupLocator(By.CSS_SELECTOR, '.search-result')
