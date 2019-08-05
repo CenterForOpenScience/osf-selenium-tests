3d82a3e09b1c5ab611453e81c07e64f34908b00a
Joshua Hernandez <jh27539@vt.edu>
2019-08-05 15:31:34 -0400

    Implement dictionary search across all tests

--- tests/test_project_files.py
+++ tests/test_project_files.py
@@ -3,7 +3,6 @@ import ipdb
 import time
 #import markers
 #import settings
-import requests
 
 from api import osf_api
 from pages.project import FilesPage
@@ -12,31 +11,23 @@ from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium import webdriver
 
-
-def find_addon_row(files_page, target_file):
-    files_page.goto()
-    start_time = time.time()
-    files_page.first_file.present()  # checks files have loaded
-    file_present_time = time.time()
-
-    #return files_page
-    for row in files_page.fangorn_rows:
-        if row.text == target_file:
-            end_time = time.time()
-            elapsed_time = end_time - start_time
-            found_time = file_present_time - start_time
-            print("Elapsed Time: ", elapsed_time)
-            print("Found Time: ", found_time)
-            return row
-    return;
-
-def click_addon_row(files_page, target_file):
-    row = find_addon_row(files_page, target_file)
-    assert row is not None
-    row.find_element_by_xpath('../..').click()
-    return;
+def format_provider_name(row):
+    if row.text.startswith('Box:'):
+        provider = 'box'
+    elif row.text.startswith('Dropbox:'):
+        provider = 'dropbox'
+    elif row.text.startswith('Amazon S3:'):
+        provider = 's3'
+    elif row.text.startswith('ownCloud'):
+        provider = 'owncloud'
+    elif row.text.startswith('osfstorage'):
+        provider = 'osf'
+    elif row.text.startswith('OSF'):
+        provider = 'osf'
+    else:
+        provider = 'provider name not found :('
+    return provider;
 
 # Click a button in the toolbar, just pass in the name
 def click_button(driver, button_name):
@@ -47,18 +38,7 @@ def click_button(driver, button_name):
             break
     return;
 
-def click_addon_folder(files_page, target_file):
-    files_page.goto()
-    time.sleep(4)
-
-    for row in files_page.fangorn_folders:
-        if row.find_element_by_xpath('../../..').text == target_file:
-            row.click()
-            break;
-    return;
-
-def create_dictionary( driver):
-
+def create_dictionary(driver):
     # Wait until fangorn has loaded all files in the tree before testing
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#tb-tbody div[data-level="3"]')))
     WebDriverWait(driver, 10).until(EC.invisibility_of_element_located((By.CSS_SELECTOR, '#tb-tbody .fa-refresh')))
@@ -70,7 +50,6 @@ def create_dictionary( driver):
 
     for row in all_fangorn_rows:
         data_level = row.get_attribute('data-level')
-        print('\ndata-level = {}: text = {}'.format(data_level, row.text))
         if data_level == '2':
             key = format_provider_name(row)
             fangorn_dictionary[key] = []
@@ -82,27 +61,19 @@ def create_dictionary( driver):
 
     return fangorn_dictionary
 
+def find_row_by_name(driver, provider, row_name):
+    all_files = create_dictionary(driver)
+    for x in all_files[provider]:
+        if x['file_name'] == row_name:
+            return x['row_object']
+        else:
+            print('Row not found! :(')
 
-
-def format_provider_name(row):
-    if row.text.startswith('Box:'):
-        provider = 'box'
-    elif row.text.startswith('Dropbox:'):
-        provider = 'dropbox'
-    elif row.text.startswith('Amazon S3:'):
-        provider = 's3'
-    elif row.text.startswith('ownCloud'):
-        provider = 'owncloud'
-    elif row.text.startswith('OSF'):
-        provider = 'osf'
-    else:
-        provider = 'provider name not found :('
-    return provider;
 
 @pytest.mark.usefixtures('must_be_logged_in')
 class TestFilesPage:
 
-    @pytest.mark.parametrize('provider', ['box', 'dropbox'])
+    @pytest.mark.parametrize('provider', ['box'])
     def test_rename_file(self, driver, default_project, session, provider):
         node_id = default_project.id
 
@@ -117,50 +88,53 @@ class TestFilesPage:
                                                       provider=provider)
 
         files_page = FilesPage(driver, guid=node_id)
+        files_page.goto()
 
-        click_addon_row(files_page, new_file)
+        row = find_row_by_name(driver, provider, new_file)
+        row.click()
         click_button(driver, 'Rename')
-
         rename_text_box = driver.find_element_by_id('renameInput')
 
         for _ in range(len(new_file)):
             rename_text_box.send_keys(Keys.BACKSPACE)
 
-        rename_text_box.send_keys('Selenium Test File')
+        new_name = 'Selenium Test File'
+        rename_text_box.send_keys(new_name)
         rename_text_box.send_keys(Keys.RETURN)
-
-        #TODO: Write another project.py->FilesPage->Locator to wait for renamed file
         time.sleep(5)
 
-        row = find_addon_row(files_page, "Selenium Test File")
-        assert row is not None
+        row = find_row_by_name(driver, provider, new_file)
+        assert row is None
 
         osf_api.delete_file(session, metadata['data']['links']['delete'].replace('foo.txt', 'Selenium Test File'))
-
 
     def test_checkout_file(self, driver, default_project, session):
         node_id = default_project.id
         provider = 'osfstorage'
 
-        # connect addon to node, upload a single test file
+        # connect add-on to node, upload a single test file
         node = osf_api.get_node(session, node_id=node_id)
         new_file, metadata = osf_api.upload_fake_file(session=session, node=node, name='checkout.txt',
                                                       provider=provider)
         files_page = FilesPage(driver, guid=node_id)
+        files_page.goto()
 
-        click_addon_row(files_page, new_file)
+        row = find_row_by_name(driver, 'osf', new_file)
+        row.click()
         click_button(driver, 'Check out file')
 
         #wait for the confirmation modal
         files_page.checkout_modal.present()
-
         driver.find_element_by_css_selector('.btn-warning').click()
-        click_addon_row(files_page, 'checkout.txt')
+        time.sleep(4)
+
+        row = find_row_by_name(driver, 'osf', new_file)
+        row.click()
         click_button(driver, 'Check in file')
 
         osf_api.delete_file(session, metadata['data']['links']['delete'])
 
-    @pytest.mark.parametrize('provider', ['box', 'dropbox', 's3', 'owncloud'])
+    @pytest.mark.parametrize('provider', ['box'])
     def test_delete_file(self, driver, default_project, session, provider):
         node_id = default_project.id
 
@@ -176,29 +150,37 @@ class TestFilesPage:
                                                       provider=provider)
 
         files_page = FilesPage(driver, guid=node_id)
+        files_page.goto()
 
-        click_addon_row(files_page, new_file)
+        row = find_row_by_name(driver, provider, new_file)
+        row.click()
         click_button(driver, 'Delete')
 
-        #wait for the delete confirmation
+        # wait for the delete confirmation
         files_page.delete_modal.present()
 
         # Front End will show 'delete failed' message - still works as expected
         driver.find_element_by_css_selector('.btn-danger').click()
+        time.sleep(4)
 
-        #Negative Test Case
-        row = find_addon_row(files_page, 'delete_this_guy.txt')
-        assert row is None
+        # Negative Test Case (test if delete_this_guy.txt is found)
+        all_files = create_dictionary(driver)
+        for x in all_files[provider]:
+            if x['file_name'] == new_file:
+                found = True
+            else:
+                found = False
+        assert found is False
 
     @pytest.mark.parametrize('provider, modifier_key, action', [
         ['box', 'none', 'move'],
-        ['box', 'alt', 'copy'],
-        ['dropbox', 'none', 'move'],
-        ['dropbox', 'alt', 'copy'],
-        ['owncloud', 'none', 'move'],
-        ['owncloud', 'alt', 'copy'],
-        ['s3', 'none', 'move'],
-        ['s3', 'alt', 'copy']
+        ['box', 'alt', 'copy']
+        # ['dropbox', 'none', 'move'],
+        # ['dropbox', 'alt', 'copy'],
+        # ['owncloud', 'none', 'move'],
+        # ['owncloud', 'alt', 'copy'],
+        # ['s3', 'none', 'move'],
+        # ['s3', 'alt', 'copy']
     ])
     def test_dragon_drop(self, driver, default_project, session, provider, modifier_key, action):
         node_id = default_project.id
@@ -217,25 +199,17 @@ class TestFilesPage:
         files_page = FilesPage(driver, guid=node_id)
         files_page.goto()
 
-        # checks files have loaded
-        files_page.first_file.present()
-
         # Find the row that contains the new file
-        for row in files_page.fangorn_rows:
-            if row.text == 'drag_this_file.txt':
-                source = row
-                break;
+        source = find_row_by_name(driver, provider, new_file)
 
         # Find the row with the OSF storage
         for row in files_page.fangorn_addons:
-            if row.text=='OSF Storage (United States)':
+            if row.text == 'OSF Storage (United States)':
                 target = row
                 break;
 
         action_chains = ActionChains(driver)
-
-        if modifier_key=='alt':
-            action_chains = ActionChains(driver)
+        if modifier_key == 'alt':
             action_chains.key_down(Keys.LEFT_ALT)
             action_chains.click_and_hold(source)
             action_chains.move_to_element(target)
@@ -250,10 +224,9 @@ class TestFilesPage:
         time.sleep(5)
 
         files_page.goto()
-        row = find_addon_row('drag_this_file.txt')
 
-        # Attempt to delete drag_this_file.txt in origin provider folder
         try:
+            # Attempt to delete drag_this_file.txt in origin provider folder
             osf_api.delete_file(session, metadata['data']['links']['delete'])
         except:
             print("No file to be deleted")
@@ -276,19 +249,14 @@ class TestFilesPage:
         files_page = FilesPage(driver, guid=node_id)
         files_page.goto()
 
-        current_files = create_dictionary(driver)
-        ipdb.set_trace()
-        download_file = current_files[provider][1]['row_object']
-        download_file.click()
+        row = find_row_by_name(driver, provider, new_file)
+        row.click()
         click_button(driver, 'Download')
         time.sleep(7)
 
         assert 'Could not retrieve file or directory' not in driver.find_element_by_xpath('/html/body').text
 
         osf_api.delete_file(session, metadata['data']['links']['delete'])
-
-        # assert response.status_code == '200'
-
 
         '''
         Next steps:
@@ -300,13 +268,10 @@ class TestFilesPage:
             - Check for a 200 status  
             
         - Dictionary
-            - # create_dictionary(files_page)
-            - change n -> 1 in the next line
-            - row = driver.find_element_by_css_selector('#tb-tbody > div > div > div:nth-child(n)')
-            - val = row.get_attribute('data-level')
-            - clean up provider & filenames
-            - update and return dictionary from function
-            
+            - replace find rows functions with dictionary search
+
+        - Drag and Drop
+            - Doesn't work for Chrome
             
         Josh Testing Notes
         Drag and Drop
