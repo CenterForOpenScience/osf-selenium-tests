b9ae5d08d869ef8791bb41e5ba1cb37edaaedf66
Joshua Hernandez <jh27539@vt.edu>
2019-10-08 10:35:33 -0400

    Remove verify() override

--- pages/cos.py
+++ pages/cos.py
@@ -1,6 +1,5 @@
 import settings
 
-from time import sleep
 from base.locators import Locator
 from pages.base import BasePage
 from selenium.webdriver.common.by import By
@@ -9,12 +8,5 @@ from selenium.webdriver.common.by import By
 class COSDonatePage(BasePage):
     url = 'https://cos.io/about/support-cos/'
 
-    identity = Locator(By.CSS_SELECTOR, 'div[class="slide-wrapper-background"', settings.LONG_TIMEOUT)
+    identity = Locator(By.CSS_SELECTOR, 'div[class="slide-wrapper-background"]', settings.LONG_TIMEOUT)
     page_heading = Locator(By.CSS_SELECTOR, 'div[class="banner-element"]')
-
-    def verify(self):
-        # TODO: Change this. This is bad
-        if settings.BUILD == 'edge':
-            sleep(.5)
-
-        return self.page_heading.text == 'Public goods infrastructure should be free to use, but it\'s not free to build and maintain.'
