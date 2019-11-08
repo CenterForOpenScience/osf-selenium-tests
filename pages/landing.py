cf99ffbcb1efd937358571197881a298790e7468
ajski <alisc262@gmail.com>
2019-11-08 11:39:06 -0500

    Add locators for testimonial carousel

--- pages/landing.py
+++ pages/landing.py
@@ -11,9 +11,15 @@ from base.locators import Locator, ComponentLocator
 class LandingPage(OSFBasePage):
     identity = Locator(By.CSS_SELECTOR, '._heroHeader_1qc5dv', settings.LONG_TIMEOUT)
 
+    # Locators
+    testimonial_carousel = Locator(By.CSS_SELECTOR, '[data-test-testimonials-container]')
+    view_research = Locator(By.CSS_SELECTOR, 'a[data-analytics-name="View research"]')
+    profile_page = Locator(By.CLASS_NAME, 'profile-fullname', settings.LONG_TIMEOUT)
+
     # Components
     navbar = ComponentLocator(EmberNavbar)
     sign_up_form = ComponentLocator(SignUpForm)
+
 
 class LegacyLandingPage(OSFBasePage):
     waffle_override = {'ember_home_page': LandingPage}
--- tests/test_landing.py
+++ tests/test_landing.py
@@ -10,14 +10,30 @@ def landing_page(driver):
     landing_page.goto()
     return landing_page
 
-
-class TestLandingPage(CreateUserMixin):
+class TestLandingPageUserCreation(CreateUserMixin):
 
     @pytest.fixture()
     def page(self, landing_page):
         return landing_page
 
+class TestLandingPage():
 
+    @pytest.fixture()
+    def page(self, landing_page):
+        return landing_page
+
+    def test_testimonial_carousel(self, page):
+        assert page.testimonial_carousel.present()
+
+# why does the test refresh multiple times?
+
+"""
+Click Get Started, confirm goes to correct page
+Search? maybe, can use existing search test parts
+Could maybe click Learn More, confrm goes to correct page
+Testimonial carousel -- test carets??? test seeing someone's research?
+
+"""
 class TestRegisteredReportsLandingPage:
 
     @markers.core_functionality
