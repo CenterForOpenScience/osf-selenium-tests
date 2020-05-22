dea2701f499f47d2468480a551bd28f0da2a565d
Josh Hernandez <jh27539@vt.edu>
2020-05-22 11:53:35 -0400

    Remove unused locator

--- pages/cos.py
+++ pages/cos.py
@@ -11,4 +11,3 @@ class COSDonatePage(BasePage):
     # This meta tag is unique to the donate page but cannot be verified as a 'visible' locator
     # See https://github.com/cos-qa/osf-selenium-tests/blob/b7f3f21376b7d6f751993cdcffea9262856263e3/base/locators.py#L157-L163
     identity = Locator(By.XPATH, '//meta[@name="cos:id" and @content="donate-page"]', settings.LONG_TIMEOUT)
-    page_heading = Locator(By.CSS_SELECTOR, 'div[class="banner-element"]')
