2db14eb9c72eb843d66d33eafc91d7200bb0fb24
DougCorell <doug@cos.io>
2021-05-25 09:58:09 -0400

    Additional Quickfiles tests: help, share, view, filter, move, delete, and rename

--- pages/quickfiles.py
+++ pages/quickfiles.py
@@ -4,6 +4,7 @@ from api import osf_api
 from selenium.webdriver.common.by import By
 
 from components.navbars import EmberNavbar
+from components.dashboard import EmberCreateProjectModal, EmberProjectCreatedModal
 from pages.base import OSFBasePage, GuidBasePage
 from base.locators import Locator, ComponentLocator, GroupLocator
 
@@ -35,9 +36,27 @@ class QuickfilesPage(BaseQuickfilesPage, GuidBasePage):
     delete_button = Locator(By.CSS_SELECTOR, '[data-test-delete-file-button]')
     move_button = Locator(By.CSS_SELECTOR, '[data-test-move-button]')
 
+    filter_input = Locator(By.CSS_SELECTOR, '[data-test-filter-input]')
+    filter_close_button = Locator(By.CSS_SELECTOR, '[data-test-close-filter]')
+    generic_modal = Locator(By.CSS_SELECTOR, '.modal-title')
+    help_modal_close_button = Locator(By.CSS_SELECTOR, '[data-test-close-current-modal]')
+    share_popover = Locator(By.CSS_SELECTOR, '[data-test-file-share-copyable-text]')
+    move_create_new_project_button = Locator(By.CSS_SELECTOR, '[data-test-ps-new-project-button]')
+    move_existing_project_button = Locator(By.CSS_SELECTOR, '[data-test-ps-existing-project-button]')
+    move_modal_close_button = Locator(By.CSS_SELECTOR, '[data-test-move-to-project-modal-close-button]')
+    confirm_delete_button = Locator(By.CSS_SELECTOR, '[data-test-delete-file-confirm-button]')
+    flash_message = Locator(By.CSS_SELECTOR, '.flash-message')
+    rename_input = Locator(By.CSS_SELECTOR, '[data-test-rename-field="rename"]')
+    rename_save_button = Locator(By.CSS_SELECTOR, '[data-test-save-rename]')
+    rename_close_button = Locator(By.CSS_SELECTOR, '[data-test-close-rename]')
+
     # Group Locators
     files = GroupLocator(By.CSS_SELECTOR, '._file-browser-item_1v8xgw')
     file_titles = GroupLocator(By.CSS_SELECTOR, '[data-test-file-item-link]')
+
+    # Components
+    create_project_modal = ComponentLocator(EmberCreateProjectModal)
+    project_created_modal = ComponentLocator(EmberProjectCreatedModal)
 
 
 class QuickfileDetailPage(BaseQuickfilesPage, GuidBasePage):
