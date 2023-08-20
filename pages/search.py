68dd6474547380711ff56c694bde80d2e5a0c3eb
DougCorell <doug@cos.io>
2023-08-20 10:14:29 -0400

    Updates for new OSF Search page

--- pages/search.py
+++ pages/search.py
@@ -11,9 +11,35 @@ from pages.base import OSFBasePage
 class SearchPage(OSFBasePage):
     url = settings.OSF_HOME + '/search/'
 
-    identity = Locator(By.ID, 'searchPageFullBar')
-    search_bar = Locator(By.ID, 'searchPageFullBar')
+    identity = Locator(By.CSS_SELECTOR, 'div[data-analytics-scope="Search page main"]')
+    search_input = Locator(
+        By.CSS_SELECTOR, '.ember-text-field.ember-view._search-input_fvrbco'
+    )
+    search_button = Locator(By.CSS_SELECTOR, 'button[data-test-search-submit]')
     loading_indicator = Locator(By.CSS_SELECTOR, '.ball-scale')
+    projects_tab_link = Locator(
+        By.CSS_SELECTOR, 'a[data-test-topbar-object-type-link="Projects"]'
+    )
+    registrations_tab_link = Locator(
+        By.CSS_SELECTOR, 'a[data-test-topbar-object-type-link="Registrations"]'
+    )
+    preprints_tab_link = Locator(
+        By.CSS_SELECTOR, 'a[data-test-topbar-object-type-link="Preprints"]'
+    )
+    files_tab_link = Locator(
+        By.CSS_SELECTOR, 'a[data-test-topbar-object-type-link="Files"]'
+    )
+    users_tab_link = Locator(
+        By.CSS_SELECTOR, 'a[data-test-topbar-object-type-link="Users"]'
+    )
+    first_card_object_type_label = Locator(By.CSS_SELECTOR, 'div._type-label_qeqpmj')
+    sort_by_button = Locator(
+        By.CSS_SELECTOR,
+        'div[data-test-topbar-sort-dropdown] > div.ember-view.ember-basic-dropdown-trigger.ember-power-select-trigger',
+    )
+    sort_by_date_newest = Locator(
+        By.CSS_SELECTOR, '#ember-basic-dropdown-wormhole > div > ul > li:nth-child(2)'
+    )
 
     # Group Locators
-    search_results = GroupLocator(By.CSS_SELECTOR, '.search-result')
+    search_results = GroupLocator(By.CSS_SELECTOR, '._result-card-container_qeqpmj')
--- tests/test_search.py
+++ tests/test_search.py
@@ -1,5 +1,8 @@
 import pytest
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 import markers
 from pages.search import SearchPage
@@ -15,8 +18,91 @@ def search_page(driver):
 class TestSearchPage:
     @markers.smoke_test
     @markers.core_functionality
-    def test_search_results_exist(self, search_page):
-        search_page.search_bar.send_keys('*')
-        search_page.search_bar.send_keys(Keys.ENTER)
+    def test_search_results_exist_all_tab(self, driver, search_page):
+        search_page.search_input.send_keys('test')
+        search_page.search_input.send_keys(Keys.ENTER)
         search_page.loading_indicator.here_then_gone()
-        assert search_page.search_results
+        WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located(
+                (By.CSS_SELECTOR, '._result-card-container_qeqpmj')
+            )
+        )
+        assert len(search_page.search_results) > 0
+
+    def test_search_results_exist_projects_tab(self, driver, search_page):
+        search_page.search_input.send_keys('test')
+        search_page.search_input.send_keys(Keys.ENTER)
+        search_page.loading_indicator.here_then_gone()
+        # Switch to the Projects Tab
+        search_page.projects_tab_link.click()
+        search_page.loading_indicator.here_then_gone()
+        WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located(
+                (By.CSS_SELECTOR, '._result-card-container_qeqpmj')
+            )
+        )
+        assert len(search_page.search_results) > 0
+        # Verify that first search result is of Project type
+        assert search_page.first_card_object_type_label.text[:7] == 'Project'
+
+    def test_search_results_exist_registrations_tab(self, driver, search_page):
+        search_page.search_input.send_keys('test')
+        search_page.search_input.send_keys(Keys.ENTER)
+        search_page.loading_indicator.here_then_gone()
+        # Switch to the Registrations Tab
+        search_page.registrations_tab_link.click()
+        search_page.loading_indicator.here_then_gone()
+        WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located(
+                (By.CSS_SELECTOR, '._result-card-container_qeqpmj')
+            )
+        )
+        assert len(search_page.search_results) > 0
+        # Verify that first search result is of Registration type
+        assert search_page.first_card_object_type_label.text[:12] == 'Registration'
+
+    def test_search_results_exist_preprints_tab(self, driver, search_page):
+        search_page.search_input.send_keys('test')
+        search_page.search_input.send_keys(Keys.ENTER)
+        search_page.loading_indicator.here_then_gone()
+        # Switch to the Preprints Tab
+        search_page.preprints_tab_link.click()
+        search_page.loading_indicator.here_then_gone()
+        WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located(
+                (By.CSS_SELECTOR, '._result-card-container_qeqpmj')
+            )
+        )
+        assert len(search_page.search_results) > 0
+        # Verify that first search result is of Preprint type
+        assert search_page.first_card_object_type_label.text == 'Preprint'
+
+    def test_search_results_exist_files_tab(self, driver, search_page):
+        search_page.search_input.send_keys('test')
+        search_page.search_input.send_keys(Keys.ENTER)
+        search_page.loading_indicator.here_then_gone()
+        # Switch to the Files Tab
+        search_page.files_tab_link.click()
+        search_page.loading_indicator.here_then_gone()
+        WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located(
+                (By.CSS_SELECTOR, '._result-card-container_qeqpmj')
+            )
+        )
+        assert len(search_page.search_results) > 0
+        # Verify that first search result is of File type
+        assert search_page.first_card_object_type_label.text == 'File'
+
+    def test_search_results_exist_users_tab(self, driver, search_page):
+        search_page.loading_indicator.here_then_gone()
+        # Switch to the Users Tab
+        search_page.users_tab_link.click()
+        search_page.loading_indicator.here_then_gone()
+        WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located(
+                (By.CSS_SELECTOR, '._result-card-container_qeqpmj')
+            )
+        )
+        assert len(search_page.search_results) > 0
+        # Verify that first search result is of User type
+        assert search_page.first_card_object_type_label.text == 'User'
